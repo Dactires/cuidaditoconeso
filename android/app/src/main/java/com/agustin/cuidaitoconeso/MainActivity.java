ff435bee8b4d4acdefc8a3ea560a259d23c0fba4
Agustin Quiroga <kiritoagustinquiroga@gmail.com>
2025-12-04 15:20:02 +0000

    Initial commit

---
+++ android/app/src/main/java/com/agustin/cuidaitoconeso/MainActivity.java
@@ -0,0 +1,5 @@
+package com.agustin.cuidaitoconeso;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.agustin.cuidaitoconeso',
+  appName: 'Cuidaito Con Eso',
+  webDir: 'out'
+};
+
+export default config;
